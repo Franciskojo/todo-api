291563b9941bb51acda149db401ca2d94e9d355f
Francis Kojo Amekeh <franciskojoamekeh@gmail.com>
2024-10-15 14:09:55 +0000

    modifield

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,10 +3,18 @@ import { multerSaveFilesOrg } from "multer-savefilesorg";
 
 export const localUpload = multer({ dest: "upload/" });
 
-export const remoteUpload = multer({
+export const todoIconUpload = multer({
     storage: multerSaveFilesOrg({
         apiAccessToken: process.env.SAVEFILESORG_API_KEY,
-        relativePath: "/uploads/*"
+        relativePath: "/todo-api/todos/*"
+    }),
+    preservePath: true
+});
+
+export const userAvatarUpload = multer({
+    storage: multerSaveFilesOrg({
+        apiAccessToken: process.env.SAVEFILESORG_API_KEY,
+        relativePath: "/todo-api/users/*"
     }),
     preservePath: true
 });
--- routes/todo.js
+++ routes/todo.js
@@ -5,13 +5,13 @@ import {
   getTodos,
   updatedTodo,
 } from "../controllers/todo.js";
-import { localUpload, remoteUpload } from "../middleware/upload.js";
+import { localUpload, todoIconUpload } from "../middleware/upload.js";
 
 // create a router
 const todoRouter = Router();
 
 // Define routes
-todoRouter.post("/todos", remoteUpload.single("icon"), addTodo);
+todoRouter.post("/todos", todoIconUpload.single("icon"), addTodo);
 
 todoRouter.get("/todos", getTodos);
 
